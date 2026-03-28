5ade91bd1b1ba5b9cd8323a63d788b716e3f64af
dnatisdale <dant.grnt@gmail.com>
2026-03-27 18:35:12 -0700

    icons

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-const BUILD_TIME = "BUILD V.84 <span class=\"accent-dash\">—</span> 27MAR2026 <span class=\"accent-dash\">—</span> 15:45";
+const BUILD_TIME = "BUILD V.85 <span class=\"accent-dash\">—</span> 27MAR2026 <span class=\"accent-dash\">—</span> 18:35";
 const audio = document.getElementById("audio");
 const fileInput = document.getElementById("fileInput");
 const urlInput = document.getElementById("urlInput");
